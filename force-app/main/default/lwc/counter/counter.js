ddb5db4153129663f6231d0eb163952e2ca239d6
Author <actions@github.com>
2024-01-05 15:50:12 +0000

    Apply apex-prettier changes

--- force-app/main/default/lwc/counter/counter.js
+++ force-app/main/default/lwc/counter/counter.js
@@ -1,13 +1,13 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class Counter extends LightningElement {
-  @api startingCount=0;
+  @api startingCount = 0;
   count;
 
   connectedCallback() {
-    this.count=this.startingCount;
+    this.count = this.startingCount;
   }
-  
+
   addOne() {
     this.count++;
     this.checkCount();
@@ -20,7 +20,6 @@ export default class Counter extends LightningElement {
   checkCount() {
     let span = this.template.querySelector(".count");
     if (this.count < 0) {
-      
       span.classList.add("negative");
     } else {
       span.classList.remove("negative");
